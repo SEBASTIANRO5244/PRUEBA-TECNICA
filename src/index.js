21dc7ab828008fe46fd6f11c71c678a8a0ac0d8f
sebastianrodriguezw <sebastian.rodriguez@wowdesarrollos.com>
2022-08-04 01:09:31 -0500

    Prueba tecnica

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LoginC } from "./containers/Login";
+import { DashboardC } from "./containers/Dasboard";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { IncidentsC } from "./containers/Incidents";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LoginC />}></Route>
+      <Route path="incidents" element={<IncidentsC />}></Route>
+      <Route path="createIncidents" element={<IncidentsC />}></Route>
+      <Route path="/dashboard" element={<DashboardC />}></Route>
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
